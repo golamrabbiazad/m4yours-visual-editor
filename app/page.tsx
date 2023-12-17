fe4ecdfc5e7e9a1d4418ecdcc4b5f1bdd3cbe911
Golamrabbi Azad <golamrabbiazad@gmail.com>
2023-12-17 16:33:39 +0600

    feat: minimal homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,56 @@
 import Link from "next/link"
+import { LogIn } from "lucide-react"
 
-import { auth } from "./api/auth/[...nextauth]/auth"
-
-export const runtime = "edge"
-
-export default async function Home() {
-  const session = await auth()
-
+export default function Home() {
   return (
     <div>
-      <header>
-        <nav>Homepage nav</nav>
+      <header className="absolute inset-x-0 top-0 z-50">
+        <nav
+          className="flex items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
+          <div className="flex lg:flex-1">
+            <Link href="/" className="-m-1.5 p-1.5">
+              <h1 className="text-xl font-bold">M4yours Editor</h1>
+            </Link>
+          </div>
+
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <Link
+              href="/auth/signin"
+              className="flex items-center gap-2 text-sm font-semibold leading-6 text-white"
+            >
+              Log in <LogIn className="h-4 w-4" />
+            </Link>
+          </div>
+        </nav>
       </header>
 
       <main>
-        <h1 className="p-16 text-white">
-          No User
-          {session && (
-            <span className="text-white">User: {session.user!.name}</span>
-          )}
-        </h1>
-        <Link scroll={false} href="/editor" className="text-black">
-          Editor
-        </Link>
+        {/* Hero section */}
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+            <div className="text-center">
+              <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                Build website with confidence
+              </h1>
+              <p className="mt-6 text-lg leading-8 text-gray-300">
+                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+                fugiat aliqua.
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6">
+                <Link
+                  href="/auth/signin"
+                  className="rounded-md bg-button px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                >
+                  Get started
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
-      <footer>footer</footer>
     </div>
   )
 }
